5b883fdc5c7a22a526638308c0e49447062e7943
p <_>
2017-05-01 14:32:32 +0900

    change algorithm to abbrev git branch names

--- emacs/init.el
+++ emacs/init.el
@@ -324,7 +324,8 @@
         (insert-file-contents head-path)
         (goto-char (point-min))
         (search-forward-regexp "\\(?:[^/]+/\\)?\\([^/\n]+\\)$" nil t)
-        (let ((str (replace-regexp-in-string "\\(.\\)[aeiouAEIOU]" "\\1" (match-string 1))))
+        (let ((str (replace-regexp-in-string
+                    "\\([aeiouAEIOU]\\)[aeiouAEIOU]" "\\1" (match-string 1))))
           (if (> (length str) 3) (substring str 0 3) str))))))
 
 (defun my-read-font-family ()
