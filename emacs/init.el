7cbace1bb0dc0b38714dc3ce406f815c7c436acb
p <_>
2018-03-12 11:48:28 +0900

    Make my-generate-random-str insert the result when called interactively

--- emacs/init.el
+++ emacs/init.el
@@ -328,7 +328,7 @@ cons of two integers."
          (max (length chars))
          (lst (make-list len nil))
          (str (mapconcat (lambda (x) (char-to-string (aref chars (random max)))) lst ""))
-         (outputfn (if (called-interactively-p 'any) 'print 'identity)))
+         (outputfn (if (called-interactively-p 'any) 'insert 'identity)))
     (funcall outputfn str)))
 
 ;; + | System
