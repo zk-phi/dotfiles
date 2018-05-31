e2fd4d7fde43147b843cc9ba82673e82ba02a9ab
p <_>
2018-05-31 11:54:05 +0900

    Add new theme icecream

--- emacs/init.el
+++ emacs/init.el
@@ -5422,10 +5422,16 @@ displayed, use substring of the buffer."
   ;;   "#000000" "#007100" "#00c000" "#00b000" "#00ca00"
   ;;   "#00b000" "#00b000" "#00ca00" "#00df00" "#00c000")
 
-  ;; less colorful "planet" theme based on "monochrome"
+  ;; ;; less colorful "planet" theme based on "monochrome"
+  ;; (my-elemental-theme-apply-colors
+  ;;   "#192129" "#7d7d7d" "#e0b776" "#729fcf" "#ff8683"
+  ;;   "#c0c0c0" "#c0c0c0" "#c0c0c0" "#649d8a" "#9e9e9e")
+
+  ;; icecream
+  ;; https://www.pinterest.jp/pin/176555247871619456
   (my-elemental-theme-apply-colors
-    "#192129" "#7d7d7d" "#e0b776" "#729fcf" "#ff8683"
-    "#c0c0c0" "#c0c0c0" "#c0c0c0" "#649d8a" "#9e9e9e")
+    "#192129" "#7d7d7d" "#fef187" "#f8c3a4" "#f9c9cf"
+    "#c0c0c0" "#c0c0c0" "#c0c0c0" "#aee2c9" "#9e9e9e")
 
   ;; switch mode-line color while recording macros
   (defadvice kmacro-start-macro (after my-recording-mode-line activate)
