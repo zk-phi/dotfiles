9e58a2b4c2c6946682934b8b91d87c5125cd5e8a
p <_>
2018-06-01 12:13:50 +0900

    Comment-out symon settings

--- emacs/init.el
+++ emacs/init.el
@@ -202,9 +202,9 @@
   (! (concat my-dat-directory "ispell-repl"))
   "File name of personal ispell replacement dictionary.")
 
-(defconst my-lingr-log-file
-  (! (concat my-dat-directory "lingr"))
-  "File to save `symon-lingr' log in.")
+;; (defconst my-lingr-log-file
+;;   (! (concat my-dat-directory "lingr"))
+;;   "File to save `symon-lingr' log in.")
 
 ;; Howm Datas
 
@@ -882,24 +882,24 @@ unary operators which can also be binary."
 (setup "commentize-conflict"
   (add-hook 'prog-mode-hook 'commentize-conflict-mode))
 
-(!-
- (setup "symon"
-   (setq symon-sparkline-ascent (!if my-home-system-p 97 100))
-   (symon-mode 1)
-   ;; (when my-lingr-account
-   ;;   (setup-in-idle "symon-lingr")
-   ;;   (setup-after "symon-lingr"
-   ;;     (setup-after "popwin"
-   ;;       (push '("*symon-lingr*") popwin:special-display-config))
-   ;;     (setq symon-lingr-user-name (car my-lingr-account)
-   ;;           symon-lingr-password  (cdr my-lingr-account)
-   ;;           symon-lingr-log-file  my-lingr-log-file
-   ;;           symon-lingr-app-key   "pvCm1t")
-   ;;     (add-to-list 'symon-monitors 'symon-lingr-monitor t)
-   ;;     ;; restart symon
-   ;;     (symon-mode -1)
-   ;;     (symon-mode 1)))
-   ))
+;; (!-
+;;  (setup "symon"
+;;    (setq symon-sparkline-ascent (!if my-home-system-p 97 100))
+;;    (symon-mode 1)
+;;    ;; (when my-lingr-account
+;;    ;;   (setup-in-idle "symon-lingr")
+;;    ;;   (setup-after "symon-lingr"
+;;    ;;     (setup-after "popwin"
+;;    ;;       (push '("*symon-lingr*") popwin:special-display-config))
+;;    ;;     (setq symon-lingr-user-name (car my-lingr-account)
+;;    ;;           symon-lingr-password  (cdr my-lingr-account)
+;;    ;;           symon-lingr-log-file  my-lingr-log-file
+;;    ;;           symon-lingr-app-key   "pvCm1t")
+;;    ;;     (add-to-list 'symon-monitors 'symon-lingr-monitor t)
+;;    ;;     ;; restart symon
+;;    ;;     (symon-mode -1)
+;;    ;;     (symon-mode 1)))
+;;    ))
 
 ;; + | Commands
 ;;   + english <-> japanese dictionary [sdic]
