8c86fd08da3f203aa31270933b702c548943aafc
zk-phi <zk-phi@users.noreply.github.com>
2020-05-05 00:29:13 +0900

    Make bytecompiler more pedantic

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -537,7 +537,7 @@ cons of two integers which defines a range of the codepoints."
 
 ;; settings for the byte-compiler
 (eval-when-compile
-  (setq byte-compile-warnings '(not make-local)
+  (setq byte-compile-warnings t
         byte-compile-dynamic  t))
 
 ;; make the main frame maximized by default
