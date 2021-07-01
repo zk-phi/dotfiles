f41683ee4e416ab186757742323dbf100cfa651f
zk-phi <zk-phi@users.noreply.github.com>
2021-07-02 01:39:54 +0900

    Revert "Remove .tool-versions"
    This reverts commit 9d5f7221a3bc3ee4b14b948c0a51a297786f51a8.

--- link.sh
+++ link.sh
@@ -17,3 +17,4 @@ rm -r /Applications/Emacs.app/Contents/Resources/site-lisp
 ln -s -i $(PWD)/emacs/site-lisp /Applications/Emacs.app/Contents/Resources/site-lisp
 
 ln -s -i $(PWD)/asdf/.asdfrc ~/.asdfrc
+ln -s -i $(PWD)/asdf/.tool-versions ~/.tool-versions
