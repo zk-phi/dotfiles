a0915c981c5fc05d2fb708979e6f2cb74758aacc
zk-phi <zk-phi@users.noreply.github.com>
2022-02-07 20:52:32 +0900

    Fix window size restoring

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,20 +1,36 @@
 hs.window.animationDuration = 0
 
-savedFrameSizes = {}
+savedFrames = {}
+lastKnownFrames = {}
+
+function sameFrame (f1, f2)
+  if f1 and
+     f2 and
+     math.abs(f1.x - f2.x) == 0 and
+     math.abs(f1.y - f2.y) == 0 and
+     math.abs(f1.h - f2.h) == 0 and
+     math.abs(f1.w - f2.w) == 0 then
+     return true
+   else
+     return false
+   end
+end
 
 function moveWindow (position)
   local window = hs.window.focusedWindow()
-  if savedFrameSizes[window:id()] == nil then
-    savedFrameSizes[window:id()] = window:frame()
+  local frame = window:frame()
+  if not sameFrame(frame, lastKnownFrames[window:id()]) then
+    savedFrames[window:id()] = frame
   end
   window:move(position, nil, true)
+  lastKnownFrames[window:id()] = window:frame()
 end
 
 function restoreWindow ()
   local window = hs.window.focusedWindow()
-  if savedFrameSizes[window:id()] then
-    window:setFrame(savedFrameSizes[window:id()])
-    savedFrameSizes[window:id()] = nil
+  if savedFrames[window:id()] then
+    window:setFrame(savedFrames[window:id()])
+    lastKnownFrames[window:id()] = savedFrames[window:id()]
   end
 end
 
