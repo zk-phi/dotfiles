97f0809d75e745fc1d089d016d3ae56fda5d8d69
zk-phi <zk-phi@users.noreply.github.com>
2023-10-09 23:12:11 +0900

    Add cheatsheet to the hammerspoon script

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,3 +1,19 @@
+-- Window manipulation commands
+
+-- default Mac commands:
+--   C-left  next desktop
+--   C-right previous desktop
+--   C-up    List apps
+--   C-down  List windows
+--   M-tab   Cycle through apps
+--   C-M-f   Toggle fullscreen
+
+-- extra commands added by this script
+--   C-M-left  Snap window to left
+--   C-M-right Snap window to right
+--   C-M-up    Maximize window
+--   C-M-down  Restore window size
+
 hs.window.animationDuration = 0
 hs.alert.defaultStyle.textSize = 16
 hs.alert.defaultStyle.radius = 8
