f1bfa3a1b5e96bf68723d88eb2875c9199c562ac
zk-phi <zk-phi@users.noreply.github.com>
2024-05-18 01:56:24 +0900

    Make modeline slightly thicker

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3217,8 +3217,8 @@ unary operators which can also be binary."
               'face 'mode-line-annotation-face))
 
 (defconst my-mode-line--vertical-spacer
-  (! (concat (propertize " " 'display '(raise +0.25))
-             (propertize " " 'display '(raise -0.25)))))
+  (! (concat (propertize " " 'display '(raise +0.375))
+             (propertize " " 'display '(raise -0.375)))))
 
 (defun my-generate-mode-line-format ()
   (let* ((lstr
