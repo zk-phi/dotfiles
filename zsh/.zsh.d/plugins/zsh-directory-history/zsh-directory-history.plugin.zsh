d715029d3d85d31fabc3a818e218ce065aa3e25e
zk-phi <zk-phi@users.noreply.github.com>
2025-12-22 16:09:02 +0900

    Rename plugin

---
+++ zsh/.zsh.d/plugins/zsh-directory-history/zsh-directory-history.plugin.zsh
@@ -0,0 +1,36 @@
+# Very minimal per-directory-history implementation
+# Based on https://github.com/ericfreese/zsh-cwd-history ((C) Eric Freese / MIT)
+#
+# This implementation just switches and loads HIST_FILE on `cd`,
+# and does nothing further. This avoids reimplementation of
+# history-management mechanism and leaves most of the work to the zsh.
+#
+# Limitation: no global/local toggle feature.
+
+HISTORY_BASEDIR="$HOME/.zsh_directory_history"
+
+_zsh_directory_history_file_name () {
+    echo "$HISTORY_BASEDIR/$(echo "${PWD:A}" | md5 -q)"
+}
+
+# Switch to a new history file, writing to the old one
+_zsh_directory_history_switch_histfile () {
+    local new_histfile=$1
+
+    # Write old histfile
+    fc -P
+
+    # Read new histfile
+    fc -p "$new_histfile"
+}
+
+# Load history for current working directory when changing dirs
+_zsh_directory_history_directory_changed () {
+    _zsh_directory_history_switch_histfile $(_zsh_directory_history_file_name)
+}
+
+mkdir -p "$HISTORY_BASEDIR"
+_zsh_directory_history_directory_changed
+
+autoload -Uz add-zsh-hook
+add-zsh-hook chpwd _zsh_directory_history_directory_changed
