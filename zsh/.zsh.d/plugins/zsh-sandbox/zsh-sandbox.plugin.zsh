5f3e2bb8360531e1e122d6862fa6a66b08f05d3c
zk-phi <zk-phi@users.noreply.github.com>
2026-03-29 05:24:22 +0900

    Add read-only restricted shell

--- zsh/.zsh.d/plugins/zsh-sandbox/zsh-sandbox.plugin.zsh
+++ zsh/.zsh.d/plugins/zsh-sandbox/zsh-sandbox.plugin.zsh
@@ -12,6 +12,18 @@ function sb () {
     fi
 }
 
+function sbro () {
+    if [[ "$RESTRICTED_SHELL" == "1" ]]; then
+        echo "sbro: Sandbox already started."
+    else
+        RESTRICTED_SHELL=1 sandbox-exec -f "$HOME/.zsh.d/plugins/zsh-sandbox/restricted-readonly-shell.sb" -D RSH_ROOT="$(pwd)" -D HOME="$HOME" "$SHELL"
+        # Update history
+        fc -R
+        # Restore current directory
+        cd "$(</tmp/rsh-pwd)"
+    fi
+}
+
 function _exit_sb () {
     if [[ "$RESTRICTED_SHELL" == "1" ]]; then
         pwd > /tmp/rsh-pwd
